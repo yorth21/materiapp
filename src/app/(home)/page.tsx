12712ab783557576738559e5eb41ae44d3120f6e
Yorth H. Ortegon <yorth@udenar.edu.co>
2024-12-04 23:35:42 -0500

    fix: layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,13 +2,10 @@ import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import {
   Tabs,
   TabsContent,
