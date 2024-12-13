e15cccb91ae38d0565c7cbddbe10610bc6db57e1
Yorth H. Ortegon <yorth@udenar.edu.co>
2024-12-13 00:19:53 -0500

    feat: version beta

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,21 +3,35 @@
 import { useState } from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
-import { ScrollArea } from '@/components/ui/scroll-area'
 import { Book, GraduationCap, Calendar, Award } from 'lucide-react'
 import { ICourse } from '@/types/course'
 import { ISemester } from '@/types/semester'
 import { getSemesters } from '../services/semester'
+import { Progress } from '@/components/ui/progress'
 
 export default function CourseManager() {
   const [semesters] = useState<ISemester[]>(getSemesters())
+  const [coursesSelected, setCoursesSelected] = useState<ICourse[]>([])
 
-  const getTotalCredits = (courses: ICourse[]) => {
+  const selectCurse = (course: ICourse) => {
+    if (coursesSelected.includes(course)) {
+      setCoursesSelected(coursesSelected.filter((c) => c !== course))
+      return
+    }
+
+    setCoursesSelected([...coursesSelected, course])
+  }
+
+  const getTotalCreditsApproved = (courses: ICourse[]) => {
+    return courses.reduce((total, course) => total + course.credits, 0)
+  }
+
+  const getTotalCreditsBySemester = (courses: ICourse[]) => {
     return courses.reduce((total, course) => total + course.credits, 0)
   }
 
   const getTotalProgramCredits = (semesters: ISemester[]) => {
-    return semesters.reduce((total, semester) => total + getTotalCredits(semester.courses), 0)
+    return semesters.reduce((total, semester) => total + getTotalCreditsBySemester(semester.courses), 0)
   }
 
   return (
@@ -25,23 +39,30 @@ export default function CourseManager() {
       <header className="mb-8 text-center">
         <h1 className="text-4xl font-bold mb-2">Materiapp</h1>
         <p className="text-muted-foreground">
-          Gestiona tus materias y créditos por semestre
+          Selecciona las materias que ya has aprobado
         </p>
       </header>
 
       <Card className="mb-8">
         <CardHeader>
-          <CardTitle className="flex items-center justify-between">
+          <CardTitle className="flex flex-col sm:flex-row gap-4 sm:gap-0 items-center justify-between">
             <div className="flex items-center gap-2">
               <Award className="h-6 w-6 text-primary" />
               <span>Resumen del Programa</span>
             </div>
             <span className="text-lg font-semibold">
-              Total de créditos: {getTotalProgramCredits(semesters)}
+            Total de créditos: {getTotalCreditsApproved(coursesSelected)} / {getTotalProgramCredits(semesters)}
             </span>
           </CardTitle>
         </CardHeader>
       </Card>
+
+      <div className="mb-6 flex items-center justify-center gap-2">
+        <Progress value={(getTotalCreditsApproved(coursesSelected) * 100) / getTotalProgramCredits(semesters)} className="w-[60%]" />
+        <span className="text-muted-foreground">
+          {Math.round((getTotalCreditsApproved(coursesSelected) * 100) / getTotalProgramCredits(semesters))}%
+        </span>
+      </div>
 
       <Accordion type="single" collapsible className="w-full">
         {semesters.map((semester) => (
@@ -52,10 +73,10 @@ export default function CourseManager() {
                   <Calendar className="h-5 w-5 text-primary" />
                   <span className="font-semibold">Semestre {semester.number}</span>
                 </div>
-                <div className="flex items-center gap-2">
+                <div className="flex justify-center items-center gap-2">
                   <GraduationCap className="h-5 w-5 text-muted-foreground" />
                   <span className="text-muted-foreground">
-                    {getTotalCredits(semester.courses)} créditos
+                    {getTotalCreditsBySemester(semester.courses)} créditos
                   </span>
                 </div>
               </div>
@@ -65,12 +86,19 @@ export default function CourseManager() {
                 <CardContent className="p-4">
                   <div className="grid gap-4">
                     {semester.courses.map((course, index) => (
-                      <div key={index} className="flex items-center justify-between p-2 rounded-md hover:bg-accent">
+                      <div key={index} className={`flex items-center justify-between p-2 gap-4 rounded-md hover:bg-accent transition-colors ${coursesSelected.includes(course) ? 'bg-accent' : ''}`}
+                        onClick={() => selectCurse(course)}
+                      >
                         <div className="flex items-center gap-3">
-                          <Book className="h-4 w-4 text-primary" />
+                          <div className="h-6 w-6 flex items-center justify-center">
+                            {coursesSelected.includes(course) 
+                              ? <GraduationCap className="h-4 w-4 text-primary" />
+                              : <Book className="h-4 w-4 text-primary" />
+                            }
+                          </div>
                           <span className="font-medium">{course.name}</span>
                         </div>
-                        <span className="px-2 py-1 bg-primary/10 text-primary rounded-md text-sm">
+                        <span className="px-2 py-1 text-primary text-nowrap rounded-md text-sm bg-primary/10">
                           {course.credits} créditos
                         </span>
                       </div>
